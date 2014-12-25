a4d1f1affe1395a8ad3f525e2363d74fe45d382c
Thiago Salles <tsmlima@gmail.com>
2014-12-25 19:10:19 -0200

    Improving database and email

--- db/migrate/001_create_ads.rb
+++ db/migrate/001_create_ads.rb
@@ -1,11 +1,15 @@
 class CreateAds < ActiveRecord::Migration
 	def change
 		create_table :ads do |t|
-			t.integer :ad_id
-			t.string  :title
+			t.string  :provider, :null => false
+			t.integer :ad_id,    :null => false
+			t.string  :title,    :null => false
+			t.string  :url,      :null => false
+			t.float   :price,    :null => false
+			t.boolean :notified, :null => false, :default => false
 
 			t.timestamps :null => false
 		end
-		add_index :ads, :ad_id, :unique => true
+		add_index :ads, [:provider, :ad_id], :unique => true
 	end
 end
