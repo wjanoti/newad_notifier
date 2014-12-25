f0d3a41a690766ba03b18645feb89c5d0f79ba6d
Thiago Salles <tsmlima@gmail.com>
2014-12-25 17:25:27 -0200

    Using tab instead of spaces for indentation

--- db/migrate/001_create_ads.rb
+++ db/migrate/001_create_ads.rb
@@ -1,11 +1,11 @@
 class CreateAds < ActiveRecord::Migration
-  def change
-    create_table :ads do |t|
-      t.integer  :ad_id
-      t.string   :title
+	def change
+		create_table :ads do |t|
+			t.integer :ad_id
+			t.string  :title
 
-      t.timestamps :null => false
-    end
-    add_index :ads, :ad_id, :unique => true
-  end
+			t.timestamps :null => false
+		end
+		add_index :ads, :ad_id, :unique => true
+	end
 end
